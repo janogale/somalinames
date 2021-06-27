f16c30a068a27cf91e35240bdf5f5b3c8320b0cb
Mukhtar Mahamed <mukhtar.mahomed@gmail.com>
2021-06-27 11:57:15 +0300

    Add global style

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,21 +1,43 @@
-import { extendTheme } from '@chakra-ui/react'
-import { createBreakpoints } from '@chakra-ui/theme-tools'
+import { extendTheme } from "@chakra-ui/react";
+import { createBreakpoints } from "@chakra-ui/theme-tools";
 
-const fonts = { mono: `'Menlo', monospace` }
+const fonts = { mono: `'Menlo', monospace` };
 
 const breakpoints = createBreakpoints({
-  sm: '40em',
-  md: '52em',
-  lg: '64em',
-  xl: '80em',
-})
+  sm: "40em",
+  md: "52em",
+  lg: "64em",
+  xl: "80em",
+});
 
 const theme = extendTheme({
   colors: {
-    black: '#16161D',
+    black: "#16161D",
   },
   fonts,
   breakpoints,
-})
+  styles: {
+    global: {
+      // styles for the `body`
+      html: {
+        scrollBehavior: "smooth",
+      },
+      body: {
+        bg: "gray.50",
+        userSelect: "none",
+        WebkitUserSelect: "none",
+        MozUserSelect: "none",
+        msUserSelect: "none",
+      },
+      // styles for the `a`
+      a: {
+        color: "red",
+        _hover: {
+          textDecoration: "none",
+        },
+      },
+    },
+  },
+});
 
-export default theme
+export default theme;
